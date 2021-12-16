fdbaad8a82c84145d5bbae9f49deefbbdeb72491
ZhengxiangShi <58726982+ZhengxiangShi@users.noreply.github.com>
2021-12-16 16:21:00 +0000

    fix: minor typo

--- Code/TP-MANN/run.sh
+++ Code/TP-MANN/run.sh
@@ -1,10 +1,10 @@
 #!bin/bash
-python train.py --seed 0 --train_size 10000 --path cameraready_10000_00
-python train.py --seed 1 --train_size 10000 --path cameraready_10000_11
-python train.py --seed 2 --train_size 10000 --path cameraready_10000_22
-python train.py --seed 3 --train_size 10000 --path cameraready_10000_33
-python train.py --seed 4 --train_size 10000 --path cameraready_10000_44
-python train.py --seed 5 --train_size 10000 --path cameraready_10000_55
-python train.py --seed 7 --train_size 10000 --path cameraready_10000_77
-python train.py --seed 8 --train_size 10000 --path cameraready_10000_88
-python train.py --seed 9 --train_size 10000 --path cameraready_10000_99
+python train.py --seed 0 --train_size 10000 --path model_0
+python train.py --seed 1 --train_size 10000 --path model_1
+python train.py --seed 2 --train_size 10000 --path model_2
+python train.py --seed 3 --train_size 10000 --path model_3
+python train.py --seed 4 --train_size 10000 --path model_4
+python train.py --seed 5 --train_size 10000 --path model_5
+python train.py --seed 7 --train_size 10000 --path model_6
+python train.py --seed 8 --train_size 10000 --path model_7
+python train.py --seed 9 --train_size 10000 --path model_8
